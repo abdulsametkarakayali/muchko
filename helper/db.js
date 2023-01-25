bbbf672a4075d949e2b920e8e13fa6d50887ac20
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2023-01-25 17:40:48 +0300

    Ufak değişiklikler

--- helper/db.js
+++ helper/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 module.exports = () => {
-  mongoose.connect('mongodb+srv://admin:Veriyaz348484@homework.qhfbr2i.mongodb.net/homework?retryWrites=true&w=majority',
-  {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex : true}
+  mongoose.connect(process.env.mongodb ,{useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex : true}
   );
-
+  
   mongoose.connection.on('open', () => {
     // console.log('MongoDB: Connected');
   });
